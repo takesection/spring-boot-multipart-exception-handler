efc70b129741da1ef49544e74244cfd05f7dfc6c
Shigeki Shoji <shige@pigumer.gr.jp>
2016-09-03 11:01:49 +0900

    Edit copyright

--- src/main/java/jp/pigumer/FileuploadApplication.java
+++ src/main/java/jp/pigumer/FileuploadApplication.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Pigumer Group Inc.
+ * Copyright 2016 Pigumer Group.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
